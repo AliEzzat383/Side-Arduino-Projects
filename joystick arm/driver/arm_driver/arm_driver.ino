e0ab84e6960ca306e481df42a5f75ccd3bd5e58f
AliEzzat383 <52717607+AliEzzat383@users.noreply.github.com>
2024-01-24 11:31:55 +0200

    driver update
    renamed wrist servos
    adjusted ranges based on adjusted arm build
    added gradual motion to wrist and gripper

--- joystick arm/driver/arm_driver/arm_driver.ino
+++ joystick arm/driver/arm_driver/arm_driver.ino
@@ -3,34 +3,34 @@
 Servo baseServo;
 Servo shoulderServo;
 Servo elbowServo;
-Servo wristServoY;
-Servo wristServoX;
+Servo wristRollServo;
+Servo wristPitchServo;
 Servo clawServo;
 
 const int basePin = 11;
 const int shoulderPin = 10;
 const int elbowPin = 9;
-const int wristYPin = 6;
-const int wristXPin = 5;
+const int wristRollPin = 6;
+const int wristPitchPin = 5;
 const int clawPin = 3;
 
 // int theta1 = baseServo.read(); // base
 int theta2 = shoulderServo.read(); // shoulder
-int theta3 = elbowServo.read();  // elbow
+// int theta3 = elbowServo.read();  // elbow
 // int theta4 = wristServoY.read();  // wrist_y
-int theta5 = wristServoX.read(); // wrist_x
+int theta5 = wristPitchServo.read(); // wrist_x
 int theta1 = 0; // base
 // int theta2 = 0; // shoulder
-// int theta3 = 0;  // elbow
+int theta3 = 0;  // elbow
 int theta4 = 0;  // wrist_y
 // int theta5 = 0; // wrist_x
 int theta6 = 60;  // claw
 
 int range1[2] = {0, 180};  // min : max for theta1 (base)
-int range2[2] = {0, 180}; // min : max for theta2 (shoulder)
-int range3[2] = {0, 180};  // min : max for theta3 (elbow)
-int range4[2] = {0, 180};  // min : max for theta4 (wrist_y)
-int range5[2] = {0, 180};  // min : max for theta5 (wrist_x)
+int range2[2] = {0, 90}; // min : max for theta2 (shoulder)
+int range3[2] = {0, 145};  // min : max for theta3 (elbow)
+int range4[2] = {0, 180};  // min : max for theta4 (wrist_roll)
+int range5[2] = {0, 180};  // min : max for theta5 (wrist_pitch)
 int range6[2] = {5, 80};   // min : max for theta6 (claw)
 
 // Define a function to gradually move the servo
@@ -50,15 +50,15 @@ void setup() {
   baseServo.attach(basePin);
   shoulderServo.attach(shoulderPin);
   elbowServo.attach(elbowPin);
-  wristServoY.attach(wristYPin);
-  wristServoX.attach(wristXPin);
+  wristRollServo.attach(wristRollPin);
+  wristPitchServo.attach(wristPitchPin);
   clawServo.attach(clawPin);
 
   baseServo.write(theta1);
   shoulderServo.write(theta2);
   elbowServo.write(theta3);
-  wristServoY.write(theta4);
-  wristServoX.write(theta5);
+  wristRollServo.write(theta4);
+  wristPitchServo.write(theta5);
   clawServo.write(theta6);
 
   Serial.begin(115200);
@@ -126,7 +126,7 @@ void loop() {
       } else if (theta6 >= range6[1]) {
         theta6 = range6[1];
       } else {
-        theta5=theta5;
+        theta6=theta6;
       }
     if ((theta2 == 0 && theta3 > 90)||(theta2 > 120 && theta3 == 0)||(theta6 < 30)){
       Serial.println("Danger Will Robinson");
@@ -141,10 +141,14 @@ void loop() {
         // Gradually move the elbow servo
         gradualMove(elbowServo, elbowServo.read(), values[2], 10);
 
-        // Set other servos directly without gradual movement
-        wristServoY.write(values[3]);
-        wristServoX.write(values[4]);
-        clawServo.write(values[5]);
+        // Gradually move the wrist roll servo
+        gradualMove(wristRollServo, wristRollServo.read(), theta4, 10);
+
+        // Gradually move the wrist pitch servo
+        gradualMove(wristPitchServo, wristPitchServo.read(), theta5, 10);
+
+        // Gradually move the claw servo
+        gradualMove(clawServo, clawServo.read(), theta6, 10);
       }
     }
     else {
